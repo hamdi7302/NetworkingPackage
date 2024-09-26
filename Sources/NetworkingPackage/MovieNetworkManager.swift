706a681b590e5f202621f6928fa5a3b5d7ceed30
Hamdi <hamdi.bntiba@gmail.com>
2024-09-26 20:43:28 +0100

    get list of genre id Value add set it as DataModel in @EnviromentObject later to use it in different View

--- Sources/NetworkingPackage/MovieNetworkManager.swift
+++ Sources/NetworkingPackage/MovieNetworkManager.swift
@@ -29,12 +29,37 @@ public enum TrendingType: String {
     case week
 }
 
+
+public struct Genres: Decodable {
+    let genres: [Genre]
+}
+
+// MARK: - Genre
+public struct Genre: Decodable {
+    let id: Int
+    let name: String
+}
+
+public enum GenreType: String {
+    case tv
+    case movie
+}
+
 protocol AppService {
     func fetchAllTrending (trendingType: TrendingType) -> AnyPublisher<TrendingMovies,NetworkError>
     func fetchImage(posterPath : String) -> AnyPublisher <UIImage?,NetworkError>
+    func fetchMoviesGenre(forMovie: GenreType) -> AnyPublisher<Genres,NetworkError>
 }
 
 public class MovieNetworkManager: AppService {
+    
+    // inclsue as @EnvironmentObject  in Datamodel  with the type tv or movies
+    public func fetchMoviesGenre(forMovie: GenreType) -> AnyPublisher<Genres,NetworkError> {
+        var endoint = "'https://api.themoviedb.org/3/genre/\(forMovie)/list?language=en'"
+        endoint.append("?api_key=\(apiKey)") // to remove and append header user session
+        // to replace token with user token
+        return networkManager.request(endoint: endoint)
+    }
     
     public func fetchImage(posterPath: String) -> AnyPublisher <UIImage?,NetworkError> {
         var endoint = "https://image.tmdb.org/t/p/w1280\(posterPath)"
